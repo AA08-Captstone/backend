288926bd6f86ca6ecd3ade670320fa77b3c9fd36
d27khan <daniel.khan_@hotmail.com>
2023-01-26 20:37:53 -0500

    Optimized for frontend dev

--- app.py
+++ app.py
@@ -1,10 +1,28 @@
 from flask import Flask,request, redirect, jsonify, render_template, url_for
+from flask_assets import Bundle, Environment
+import os
 
 app = Flask(__name__)
 
-@app.route("/")
+assets = Environment(app)
+css = Bundle("src/main.css", output="dist/main.css")
+js = Bundle("src/*.js", output="dist/main.js") # new
+
+assets.register("css", css)
+assets.register("js", js) # new
+css.build()
+js.build() # new
+
+
+@app.route("/",methods=['GET','POST'])
 def index():
-    return render_template('index.html')
+    if request.method == "POST":
+        choice = request.form['file_selection']
+        return render_template(choice)
+    else:
+        cwd = os.getcwd()
+        files = os.listdir(f"{cwd}/templates/")
+        return render_template('index.html',files=files)
 
 
 @app.route("/Blog")
