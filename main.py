abc3f129d21ba59a80807f901c057328390c1d00
d27khan <daniel.khan_@hotmail.com>
2023-04-09 05:42:39 -0400

    Added header, FLowbite, some code changes, candidate profile isnt complete yet

--- main.py
+++ main.py
@@ -19,12 +19,40 @@ main = Blueprint('main', __name__)
 def index():
     logout_user() 
     #upload_jobs()
-    return render_template('index.html')
+    return render_template('landingPage.html')
 
+@main.route('/features')
+def features_page():
+    return render_template("features.html")
+
+@main.route('/faq') # profile page that return 'profile'
+def faq_page():
+    return render_template('faq.html')
+
+@main.route('/contact') # profile page that return 'profile'
+def contact_page():
+    return render_template('contact.html')
+
+@main.route('/about') # profile page that return 'profile'
+def about_page():
+    return render_template('about.html')
+
+@main.route('/OurTeam') # profile page that return 'profile'
+def our_team_page():
+    return render_template('about.html')
+
+@main.route('/portal/<string:method>',methods=['GET']) # profile page that return 'profile'
+def login_portal(method):
+    if method == "login":
+        return render_template('loginPortal.html',links=["/candidateProfile","/employerprofile"],text=["Candidate","Employer"])
+    elif method == "signup":
+        return render_template('loginPortal.html',links=["/signup","/EmployerSignup"],text=["I'm looking for a Job","I'm an Employer"])
+
+#Remove this??
 @main.route('/profile') # profile page that return 'profile'
 @login_required
 def profile():
-    return render_template('profile.html', name=current_user.name)
+    return render_template('profile.html')
 
 app = create_app() # we initialize our flask app using the __init__.py function
 if __name__ == '__main__':
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
   content: [
     './templates/**/*.html',
+    "https://unpkg.com/mistral-ui@1.x.x/dist/cdn.min.js",
+    "./node_modules/flowbite/**/*.js",
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require('flowbite/plugin')],
 }
