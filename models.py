7dc3c6181194ca3882abba9a305649c00533971e
d27khan <daniel.khan_@hotmail.com>
2023-04-25 14:14:27 -0400

    Admin + login issues

--- models.py
+++ models.py
@@ -3,7 +3,7 @@ from __init__ import db
 
 
 class User(UserMixin, db.Model):
-    id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
+    id = db.Column(db.Integer, primary_key=True, autoincrement = False) # primary keys are required by SQLAlchemy
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
     name = db.Column(db.String(1000))
@@ -15,14 +15,14 @@ class User(UserMixin, db.Model):
     bio = db.Column(db.String(10000))
 
 class Employer(UserMixin, db.Model):
-    id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
+    id = db.Column(db.Integer, primary_key=True, autoincrement = False) # primary keys are required by SQLAlchemy
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
     name = db.Column(db.String(1000))
     profile_setup = db.Column(db.Boolean, default=False, nullable=False)
 
 class AdminUser(UserMixin, db.Model):
-    id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
+    id = db.Column(db.Integer, primary_key=True, autoincrement = False) # primary keys are required by SQLAlchemy
     username = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
     name = db.Column(db.String(1000))
--- utils.py
+++ utils.py
@@ -1,25 +1,37 @@
 from dotenv import load_dotenv
 import os 
-from models import AdminUser,Job
+from models import AdminUser,Job,User,Employer
 from werkzeug.security import generate_password_hash, check_password_hash
 from __init__ import db
 import pandas as pd
-
+import random
 
 ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 
-
+def generate_id():   
+    while True:
+        n = random.randint(1,1000)
+        admin_user = AdminUser.query.filter_by(id=n).first()
+        candidate = User.query.filter_by(id=n).first()
+        employer = Employer.query.filter_by(id=n).first()
+        if not (admin_user or candidate or employer):
+            return n
+        
+        
 def create_super_admin():
     # creating super admin
     username = os.getenv("ADMIN_USERNAME")
     admin_check = AdminUser.query.filter_by(username=username).first() 
     if admin_check == None:
         password = os.getenv("ADMIN_PASSWORD")
-        super_admin = AdminUser(username=username,
+        super_admin = AdminUser(id=generate_id(),username=username,
                     name="Super Admin", 
                     password=generate_password_hash(password, method='sha256'))
         db.session.add(super_admin)
         db.session.commit()
+        
+
+
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
